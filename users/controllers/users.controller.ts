08d2fd115c87ebc13eba6fa9ee910915f802ffee
fjplaurr <fjplaurr@gmail.com>
2022-12-21 17:18:11 +0100

    feat: add user middleware, router, reducer & dao

---
+++ users/controllers/users.controller.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import UsersService from "../services/users.service";
+import argon2 from "argon2";
+import debug from "debug";
+
+const log: debug.IDebugger = debug("app:users-controller");
+
+class UsersController {
+  async listUsers(req: express.Request, res: express.Response) {
+    const users = await UsersService.list(100, 0);
+    res.status(200).send(users);
+  }
+
+  async getUserById(req: express.Request, res: express.Response) {
+    const user = await UsersService.readById(req.params.userId);
+    res.status(200).send(user);
+  }
+
+  async createUser(req: express.Request, res: express.Response) {
+    req.body.password = await argon2.hash(req.body.password);
+    const userId = await UsersService.create(req.body);
+    res.status(201).send({ id: userId });
+  }
+
+  async patch(req: express.Request, res: express.Response) {
+    if (req.body.password) {
+      req.body.password = await argon2.hash(req.body.password);
+    }
+    log(await UsersService.patchById(req.params.userId, req.body));
+    res.status(204).send();
+  }
+
+  async put(req: express.Request, res: express.Response) {
+    req.body.password = await argon2.hash(req.body.password);
+    log(await UsersService.putById(req.params.userId, req.body));
+    res.status(204).send();
+  }
+
+  async removeUser(req: express.Request, res: express.Response) {
+    log(await UsersService.deleteById(req.params.userId));
+    res.status(204).send();
+  }
+}
+
+export default new UsersController();
